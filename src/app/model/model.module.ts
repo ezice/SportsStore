03e173dbcb33eb797ca6bb355ce6a976db9f647e
Rick Huff <rhuff2@yahoo.com>
2024-08-12 06:41:15 -0500

    PWA running

--- src/app/model/model.module.ts
+++ src/app/model/model.module.ts
@@ -5,11 +5,12 @@ import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
 import { RestDataSource } from "./rest.datasource";
-import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+// import { provideHttpClient, withInterceptorsFromDi } from "@angular/common/http";
+import { HttpClientModule } from "@angular/common/http";
 import { AuthService } from "./auth.service";
 import { ConnectionService } from "./connection.service";
 
-@NgModule({ imports: [], providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository,
+@NgModule({ imports: [HttpClientModule], providers: [ProductRepository, StaticDataSource, Cart, Order, OrderRepository,
         { provide: StaticDataSource, useClass: RestDataSource },
         RestDataSource, AuthService, ConnectionService]
 })
